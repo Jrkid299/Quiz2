43f6166480a38bbc7e066a0dc749c0dfcfedbe47
Jalen Lamb <2018118881@ub.edu.bz>
2022-10-07 22:48:46 -0600

    elm things

--- star/src/Pic.elm
+++ star/src/Pic.elm
@@ -3,20 +3,39 @@ module Pic exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
 
-main : Html msg
-main = 
+baseUrl : String
+baseUrl = 
+    "images/"
+
+initialModel : {url : String, caption : String}
+initialModel = 
+    {
+        url = baseUrl ++ "TheKeyboard Player.gif"
+        ,caption = "me"
+    }
+
+viewPhoto : { url : String, caption : String} -> Html msg
+viewPhoto model = 
+    div [ class "detail-photo" ]
+    [
+        img [ src model.url ] []
+        , div [ class "photo-info" ]
+        [ h2 [ class "caption" ] [ text model.caption ] ]
+    ]
+
+
+view : {url : String, caption : String } -> Html msg
+view model =
     div []
     [
         div [ class "header" ]
         [ h1 [] [text "Pic" ] ]
         ,div [class "content-flow" ]
         [
-            div [ class "detail-photo" ]
-            [
-                img [ src "TheKeyboard Player.gif" ] []
-                , div [ class "photo-info" ]
-                [ h2 [ class "caption" ] [ text "Yeh Yeh "] ]
-            ]
+            viewPhoto model
         ]
     ]
 
+main : Html msg
+main =
+    view initialModel
